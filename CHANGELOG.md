03a49b583692ef812b39856254fcc3afcb9a94df
Daniel Indlekofer <di3@gmx.net>
2019-04-06 06:06:20 +0200

    testing

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,7 @@
 ## [Unreleased]
 
+* added chai - modified tests
+
 ## 0.1.0 (2019-04-05)
 
 * init alpha
--- test/index.test.js
+++ test/index.test.js
@@ -4,7 +4,7 @@ import { mount, shallow } from 'enzyme';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-import assert from 'assert';
+import { expect } from 'chai';
 
 configure({ adapter: new Adapter() });
 
@@ -13,7 +13,7 @@ import Piechart from '../src/index.jsx';
 describe('<Piechart/>', () => {
   it('should render a proper svg with paths', () => {
     const wrapper = shallow(<Piechart size={50} values={[30, 20]} colors={['#fff', '#000']} />);
-    assert(wrapper.html(), '<svg width="50" height="50"><path d="M25,25 L25,0 A25,25 0 1,1 10.305368692688171, 45.22542485937369 z" fill="#fff" transform="rotate(0, 25, 25)"></path><path d="M25,25 L25,0 A25,25 0 0,1 39.69463130731183, 45.22542485937369 z" fill="#000" transform="rotate(216, 25, 25)"></path></svg>');
+    expect(wrapper.html()).to.equal('<svg width="50" height="50"><path d="M25,25 L25,0 A25,25 0 1,1 10.305368692688171, 45.22542485937369 z" fill="#fff" transform="rotate(0, 25, 25)"></path><path d="M25,25 L25,0 A25,25 0 0,1 39.69463130731183, 45.22542485937369 z" fill="#000" transform="rotate(216, 25, 25)"></path></svg>');
   });
 
   it('should handle clicks', () => {
@@ -24,18 +24,16 @@ describe('<Piechart/>', () => {
     };
     const wrapper = shallow(<Piechart values={[10, 40, 10]} colors={['#fff', '#000', '#0f0']} onClick={onClickTest}/>);
     const pathList = wrapper.find('path');
-    assert(pathList.length, 3);
+    expect(pathList.length).to.equal(3);
     pathList.at(0).simulate('click');
     pathList.at(1).simulate('click');
     pathList.at(2).simulate('click');
-    assert(c, 3);
-    assert(i, [0, 1, 2]);
+    expect(c).to.equal(3);
+    expect(i).to.deep.equal([0, 1, 2]);
   });
-  it('should have props for values and colors', () => {
-    const wrapper = shallow(<Piechart values={[30, 20]} colors={['#fff', '#000']} />);
-    assert(typeof wrapper.props().email === 'undefined', true);
-    assert(typeof wrapper.props().values === 'undefined', false);
-    assert(typeof wrapper.props().colors === 'undefined', false);
-    assert(typeof wrapper.props().onClick === 'undefined', true);
+  it('should change size', () => {
+    const wrapper = shallow(<Piechart size={100} values={[30, 20]} colors={['#fff', '#000']} />);
+    expect(wrapper.props().height).to.equal(100);
+    expect(wrapper.props().width).to.equal(100);
   });
 });
